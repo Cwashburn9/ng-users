2fcb43db5d38b3b4673ea71b37777f3831aec67d
Caitlin Washburn <caitlinkwashburn@gmail.com>
2020-02-19 21:01:40 -0600

    Generated and routed UserCreateComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,16 @@ import { UsersComponent }   from './users/users.component';
 
 import { UserViewComponent }   from './user-view/user-view.component';
 
+import { UserCreateComponent }   from './user-create/user-create.component';
+
 // 3. Declare your routes
 const routes: Routes = [
   // 4. The default route
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   // 5. Map /users to the UsersComponent
   { path: 'users', component: UsersComponent },
-  { path: 'users/view/:id', component: UserViewComponent }
+  { path: 'users/view/:id', component: UserViewComponent },
+  { path: 'users/create', component: UserCreateComponent }
 ];
 
 @NgModule({
