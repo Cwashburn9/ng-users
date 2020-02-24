1327e587ed7a45a0e1130723e5d501c729dcab6a
Caitlin Washburn <caitlinkwashburn@gmail.com>
2020-02-24 14:09:57 -0600

    started articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,13 +12,16 @@ import { UserCreateComponent }   from './user-create/user-create.component';
 
 import { UserEditComponent }   from './user-edit/user-edit.component';
 
+import { ArticlesComponent }   from './articles/articles.component';
+
 // 3. Declare your routes
 const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
   { path: 'users/view/:id', component: UserViewComponent },
   { path: 'users/create', component: UserCreateComponent },
-  { path: 'users/edit/:id', component: UserEditComponent }
+  { path: 'users/edit/:id', component: UserEditComponent },
+  { path: 'articles', component: ArticlesComponent }
 ];
 
 @NgModule({
